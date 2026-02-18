166ff9b27bf45f17d6461946e0ff70c9e7282968
jixeis9541@gmail.com <jixeis9541@gmail.com>
2026-02-19 07:30:27 +0900

    ls -h option

--- src/user/apps/ls/main.c
+++ src/user/apps/ls/main.c
@@ -2,9 +2,37 @@
 #include "user_syscall.h"
 #include "user_path.h"
 
+static int calc_file_size(uint32_t *size_dst, char *unit_dst, const uint32_t *file_size) {
+    uint32_t unit_size;
+
+    // GB
+    unit_size = *file_size / (1024 * 1024 * 1024);
+    if (unit_size > 0) {
+        *size_dst = unit_size;
+        strcpy_s(unit_dst, 2, "G");
+        return 0;        
+    }
+    // MB
+    unit_size = *file_size / (1024 * 1024);
+    if (unit_size > 0) {
+        *size_dst = unit_size;
+        strcpy_s(unit_dst, 2, "M");
+        return 0;        
+    }    
+    // KB
+    unit_size = *file_size / 1024;
+    if (unit_size > 0) {
+        *size_dst = unit_size;
+        strcpy_s(unit_dst, 2, "K");
+        return 0;
+    }
+    return -1;
+}
+
 int main(int argc, char **argv) {
     bool detail = false;
     bool all_print = false;
+    bool human_readable = false;
 
     // parse options
     if (argc == 2 || argc == 3) {
@@ -20,6 +48,9 @@ int main(int argc, char **argv) {
                         break;
                     case 'a':
                         all_print = true;
+                        break;
+                    case 'h':
+                        human_readable = true;
                         break;
                 }
                 argv_1++;
@@ -56,7 +87,19 @@ int main(int argc, char **argv) {
             if (!all_print && ent.name[0] == '.') {
                 continue;
             }
-            printf("%s\t%d\t", ent.type == FS_TYPE_DIR ? "d" : "f", ent.size);
+            printf("%s\t", ent.type == FS_TYPE_DIR ? "d" : "-");
+            if (human_readable) {
+                uint32_t file_size;
+                char unit[2];
+                if (calc_file_size(&file_size, unit, &ent.size) < 0) {
+                    file_size = ent.size;
+                    unit[0] = '\0';
+                }
+                printf("%d%s\t", file_size, unit);
+            }
+            else {
+                printf("%d\t", ent.size);
+            }
             if (ent.type == FS_TYPE_DIR) {
                 printf("%s/\n", ent.name);
             } else {
